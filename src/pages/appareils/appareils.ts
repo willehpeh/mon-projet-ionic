ad27b5a17914ccba7049a66e92422783fa09bfad
willehpeh <openclassrooms@pc24.home>
2018-05-09 14:20:08 +0200

    P2C2A

---
+++ src/pages/appareils/appareils.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-appareils',
+  templateUrl: 'appareils.html'
+})
+export class AppareilsPage {
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AppareilsPage } from '../appareils/appareils';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  onGoToAppareils() {
+    this.navCtrl.push(AppareilsPage);
   }
 
 }
