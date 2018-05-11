5972dfbdd504b870868aca7cb1a2f667ad69ccef
willehpeh <openclassrooms@pc24.home>
2018-05-11 17:48:45 +0200

    P2C3B - Alerts

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-settings',
@@ -6,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class SettingsPage {
 
+  constructor(private alertCtrl: AlertController) {
+
+  }
+
+  onToggleLights() {
+    let alert = this.alertCtrl.create({
+      title: 'Êtes-vous certain(e) de vouloir continuer ?',
+      subTitle: 'Cette action allumera ou éteindra toutes les lumières de la maison !',
+      buttons: [
+        {
+          text: 'Annuler',
+          role: 'cancel'
+        },
+        {
+          text: 'Confirmer',
+          handler: () => console.log('Confirmé !')
+        }
+      ]
+    });
+    alert.present();
+  }
 }
