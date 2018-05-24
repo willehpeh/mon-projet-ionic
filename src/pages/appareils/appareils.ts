20cb596b9cb29f4a5004acd072eb30cd64189eeb
willehpeh <openclassrooms@pc24.home>
2018-05-24 20:20:40 +0200

    P3C2C - Base de données

--- src/pages/appareils/appareils.ts
+++ src/pages/appareils/appareils.ts
@@ -1,26 +1,35 @@
-import { Component } from '@angular/core';
-import { MenuController, ModalController, NavController } from 'ionic-angular';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { LoadingController, MenuController, ModalController, NavController, ToastController } from 'ionic-angular';
 import { SingleAppareilPage } from './single-appareil/single-appareil';
 import { Appareil } from '../../models/Appareil';
 import { AppareilsService } from '../../services/appareils.service';
 import { AppareilFormPage } from '../appareil-form/appareil-form';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'page-appareils',
   templateUrl: 'appareils.html'
 })
-export class AppareilsPage {
+export class AppareilsPage implements OnInit, OnDestroy {
 
   appareilsList: Appareil[];
+  appareilsSubscription: Subscription;
 
   constructor(private modalCtrl: ModalController,
               private appareilsService: AppareilsService,
               private menuCtrl: MenuController,
-              private navCtrl: NavController) {
+              private navCtrl: NavController,
+              private toastCtrl: ToastController,
+              private loadingCtrl: LoadingController) {
   }
 
-  ionViewWillEnter() {
-    this.appareilsList = this.appareilsService.appareilsList.slice();
+  ngOnInit() {
+    this.appareilsSubscription = this.appareilsService.appareils$.subscribe(
+      (appareils: Appareil[]) => {
+        this.appareilsList = appareils;
+      }
+    );
+    this.appareilsService.emitAppareils();
   }
 
   onLoadAppareil(index: number) {
@@ -34,6 +43,62 @@ export class AppareilsPage {
 
   onNewAppareil() {
     this.navCtrl.push(AppareilFormPage);
+  }
+
+  onSaveList() {
+    let loader = this.loadingCtrl.create({
+      content: 'Sauvegarde en cours…'
+    });
+    loader.present();
+    this.appareilsService.saveData().then(
+      () => {
+        loader.dismiss();
+        this.toastCtrl.create({
+          message: 'Données sauvegardées !',
+          duration: 3000,
+          position: 'bottom'
+        }).present();
+      }
+    ).catch(
+      (error) => {
+        loader.dismiss();
+        this.toastCtrl.create({
+          message: error,
+          duration: 3000,
+          position: 'bottom'
+        }).present();
+      }
+    );
+  }
+
+  onFetchList() {
+    let loader = this.loadingCtrl.create({
+      content: 'Récupération en cours…'
+    });
+    loader.present();
+    this.appareilsService.retrieveData().then(
+      () => {
+        loader.dismiss();
+        this.toastCtrl.create({
+          message: 'Données récupérées !',
+          duration: 3000,
+          position: 'bottom'
+        }).present();
+      }
+    ).catch(
+      (error) => {
+        loader.dismiss();
+        this.toastCtrl.create({
+          message: error,
+          duration: 3000,
+          position: 'bottom'
+        }).present();
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.appareilsSubscription.unsubscribe();
   }
 
 }
